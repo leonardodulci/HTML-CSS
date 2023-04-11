29fd9a05647ce5e074a2e82a56aa7e65ad5036ac
Leonardo Silva Dulci <leonardodulci@gmail.com>
2023-04-10 21:00:50 -0300

    formulário

--- EXERCICIOS/ex025/cadastro.php
+++ EXERCICIOS/ex025/cadastro.php
@@ -1,2 +1,14 @@
 os dados foram enviados!
 
+<h1>Exemplo de Formulário</h1>
+    <form action="cadastro.php" method="post"autocomplete="on">
+
+
+
+
+        
+   <p>
+            <input type="submit" value="Enviar">
+            <input type="reset" value="Limpar">
+        </p>
+    </form>
